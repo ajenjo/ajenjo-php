871152649446ad52de95136423e6bec1241f47b8
Jon Dotsoy <hi@jon.soy>
2015-05-28 05:03:28 -0300

    Un repositorio limpio.

--- tests/ajenjoTest.php
+++ tests/ajenjoTest.php
@@ -1,12 +1,12 @@
 <?php
 
-class ajenjoTest extends PHPUnit_Framework_TestCase
+class ajenjoTest extends \PHPUnit_Framework_TestCase
 {
   /**
    * Un test de prueba.
    **/
   public function testOne()
   {
-    $this->assertTrue(!false);
+
   }
 }
